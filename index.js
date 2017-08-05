0bad58a98271e811a6835e7babc5bebf2e6d4b33
rahul <rahulsingh2240@gmail.com>
2017-08-05 23:14:23 +0530

    add maths operations

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-var username = window.prompt("Enter your name");
-var greeting = document.getElementById("greeting");
-greeting.innerHTML += ", " + username
+// Part 3 - Update greeting with user's name
+var userName = window.prompt("Please enter your name");
+var greetingParagraph = document.getElementById("greeting");
+greetingParagraph.innerHTML += ", " + userName;
 
-var operand1 = window.prompt("Enter operand 1");
-var operand2 = window.prompt("Enter operand 2");
+// Part 4 - Get numbers from user
+var span1 = document.getElementById("operand1");
+var span2 = document.getElementById("operand2");
+var num1 = window.prompt("enter a number");
+var num2 = window.prompt("enter another number");
+span1.innerHTML = num1;
+span2.innerHTML = num2;
 
-document.getElementById("operand1").innerHTML = operand1
-document.getElementById("operand2").innerHTML = operand2
+// Part 5 - Operations
+var sum = parseInt(num2) + parseInt(num1);
+var difference = num1 - num2;
+var product = num1 * num2;
+var quotient = num1 / num2;
+var modResult = num1 % num2;
+
+// Part 6 - Display results
+var resultMessage = "<span>" + num1 + "</span> and <span>" + num2 + "</span> is ";
+document.getElementById("addition").innerHTML = "The sum of " + resultMessage + sum;
+document.getElementById("subtraction").innerHTML = "The difference between " + resultMessage + difference;
+document.getElementById("multiplication").innerHTML = "The product of " + resultMessage + product;
+document.getElementById("division").innerHTML = "The quotient of " + resultMessage + quotient;
+document.getElementById("modulus").innerHTML = "The result of the mod operation on " + resultMessage + modResult;
