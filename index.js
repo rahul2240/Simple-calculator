d43afc2effca0cd4d8ace5da79c8ce568ba014fe
rahul <rahulsingh2240@gmail.com>
2017-08-05 23:01:33 +0530

    Add name and operand

---
+++ index.js
@@ -0,0 +1,9 @@
+var username = window.prompt("Enter your name");
+var greeting = document.getElementById("greeting");
+greeting.innerHTML += ", " + username
+
+var operand1 = window.prompt("Enter operand 1");
+var operand2 = window.prompt("Enter operand 2");
+
+document.getElementById("operand1").innerHTML = operand1
+document.getElementById("operand2").innerHTML = operand2
